c2122c1c1d2d9f974421b54fed52f13710db739b
Ali <alikomurcu32430@gmail.com>
2022-06-05 18:10:05 +0300

    Main game logic is created.

--- main.c
+++ main.c
@@ -1,8 +1,43 @@
+#include <xc.h>
+#include <stdint.h>
+// CONFIG Oscillator and TURN OFF Watchdog timer
+#pragma config OSC = HSPLL
+#pragma config WDT = OFF
 
-
-
+/*_* GLOBAL DECLERATIONS GOES HERE */
 typedef enum {TEM, CDM, TSM} game_state_t;
 game_state_t game_state = TEM;
+
+bool re0Pressed, re1Pressed, re2Pressed, re3Pressed, re4Pressed, re5Pressed;
+
+/*_* Initialize variables */
+void init_vars()
+{
+    re0Pressed = re1Pressed = re2Pressed = re3Pressed = re4Pressed = re5Pressed = false;
+    game_state = TEM;
+}
+
+void init_ports()
+{
+    /*_* INPUT TRISSES*/
+    TRISE = 0x3f;       // 0011 1111 -> RE 0-5
+    TRISH = 0X10;       // 0001 0000 -> RH4
+
+
+
+    /*_* OUTPUT TRISSES*/
+    // LCD BASED TRISSES
+    // TRISB = TODO
+    // TRISD = TODO 
+    TRISA = 0X00;
+    TRISC = 0X00;
+
+    // 7-SEG BASED TRISSES
+    // PORTH IS EDITED UPWARDS
+    PORTJ = 0X00;
+
+}
+
 
 void game_task()
 {
@@ -26,12 +61,35 @@ void game_task()
 
 void input_task()
 {
+    if(PORTEbits.RE0)
+    {
+        re0Pressed = true;
+    }
+    if(PORTEbits.RE1)
+    {
+        re1Pressed = true;
+    }
+    if(PORTEbits.RE2)
+    {
+        re2Pressed = true;
+    }
+    if(PORTEbits.RE3)
+    {
+        re3Pressed = true;
+    }
+    if(PORTEbits.RE4)
+    {
+        re4Pressed = true;
+    }
+    if(PORTEbits.RE5)
+    {
+        re5Pressed = true;
+    }
     
 }
 
 int main()
 {
-
 
     init_vars();
     init_ports();
